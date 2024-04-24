c62003222df6b5f71c6324f3c6f6a2f024a4abc8
javier montoya <montoya.javier.15@hotmail.com>
2024-04-24 11:33:25 -0600

    dando de alta el ejercicio 4

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 en este proyecto se guardan los codigos para resolver los ejercicios propuestos en clase
+el repositorio se ira actualizando a medida que los ejercicios queden reseuletos
---
+++ ejercicio4.py
@@ -0,0 +1,9 @@
+'''
+Escribe un programa que muestre los números perfectos entre 1 y 1000. 
+Definición: Los números perfectos son aquellos cuya suma de sus divisores propios es igual al número. 
+Definición: Los divisores propios de un número son aquellos que dividen a un número en forma exacta, residuo igual a cero.
+Ejemplo de número perfecto: numero = 6. Divisores propios 1, 2, 3. Suma de divisores propios = 6. 6 = 6.
+'''
+
+for  num in range(1, 1000):
+    print (num)
