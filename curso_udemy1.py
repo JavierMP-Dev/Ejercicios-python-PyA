e9ae7b4383b5e1b8b7f159098ff7c150a979700e
javier montoya <montoya.javier.15@hotmail.com>
2024-04-26 12:28:22 -0600

    agregando operadores

--- curso_udemy1.py
+++ curso_udemy1.py
@@ -1,5 +1,10 @@
-import sys
+import math
+
+#print(math.factorial(9))
 
 print ("Hola !!")
 
-print(5 +6) #Suma de dos numeros enteros 
+print(5 +6) #Suma de dos numeros enteros                                      
+print(56/5) #Division entre dos numeros decimales 
+
+print(10%3) #modulo deveulve el residuo de la division
