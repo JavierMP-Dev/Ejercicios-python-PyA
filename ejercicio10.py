4e5a7fc6c6aaec998575bb02b98102d620decd92
javier montoya <montoya.javier.15@hotmail.com>
2024-04-26 08:39:10 -0600

    comentando codigo

--- ejercicio10.py
+++ ejercicio10.py
@@ -2,10 +2,10 @@
 #hacer division en el rango de [-10, 10]
 
 
-for divisor in range(-10, 10):
-    try: 
-        resultado = 20 / divisor
-    except ZeroDivisionError:
+for divisor in range(-10, 10): #recorriendo mediante ciclo for
+    try: #haciendo operacion en cada iteracion
+        resultado = 20 / divisor 
+    except ZeroDivisionError:#manejo del error si intento dividir entre cero
         print(f"No se puede dividir con el divisor {divisor} por que es cero")
-    else:
+    else:   #si no hay error salto a esta parte y continua la ejecucion
         print(f"operación división 20 {divisor}: {resultado}")
--- ejercicio6.py
+++ ejercicio6.py
@@ -1,5 +1,2 @@
 #Escribe un programa para calcular el residuo de una división en forma recursiva e iterativa. Hint: similar al anterior.
 
-num1 = int(input("Introduce el divisor: "))
-num2 = int(input("Introduce el dividendo: "))
-
