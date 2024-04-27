d87ccbeeb44e76e6b41527e646387b04ec590803
javier montoya <montoya.javier.15@hotmail.com>
2024-04-26 18:15:04 -0600

    añadiendo ciclo for de 1 al 100

--- curso_udemy1.py
+++ curso_udemy1.py
@@ -8,3 +8,18 @@ print(5 +6) #Suma de dos numeros enteros
 print(56/5) #Division entre dos numeros decimales 
 
 print(10%3) #modulo deveulve el residuo de la division
+
+#operadores logicos
+edad = 25
+experiencia = True
+
+if edad >=18 and experiencia:
+    print("Puede aplicar al trabajo")
+else:
+    print("No cumple los requisitos")
+
+
+llueve = False
+
+if not llueve:
+    print("")
